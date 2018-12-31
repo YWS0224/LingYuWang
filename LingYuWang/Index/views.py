afbd567b868cd3b22a9e7cb0a32679d03933ee29
Your Name <1312422324@qq.com>
2018-12-31 11:55:30 +0800

    整合

---
+++ LingYuWang/Index/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from LunBo.views import *
+from topType.views import *
+
+
+# Create your views here.
+
+def Index_html(request):
+    LunBo_T = Lun_Bo(request)
+    top_Type1 = top_Type(request)
+    return render(request, 'Index.html', {'LunBo_T': LunBo_T, 'TopType': top_Type1})
