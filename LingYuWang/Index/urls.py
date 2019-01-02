8b4f7057e0e3b92368a6ef454d85fcb9c4cd1980
WMF0515 <371785151@qq.com>
2019-01-01 17:52:57 -0800

    添加bootstrap

--- LingYuWang/Index/urls.py
+++ LingYuWang/Index/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
+
+from Index import views
+
 
 app_name = 'Index'
 
+
 urlpatterns = [
     path('index/', Index_html, name='index'),
+    # re_path(r'^index2(\d{1,2})', views, dash, name='index2'),
 ]
--- LingYuWang/Index/views.py
+++ LingYuWang/Index/views.py
@@ -9,3 +9,7 @@ def Index_html(request):
     LunBo_T = Lun_Bo(request)
     top_Type1 = top_Type(request)
     return render(request, 'Index.html', {'LunBo_T': LunBo_T, 'TopType': top_Type1})
+
+#
+# def dash(request):
+#     return render(request, 'Index.html')
